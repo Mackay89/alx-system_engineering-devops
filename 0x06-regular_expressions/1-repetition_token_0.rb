35ff10026846956161b759407cb7c364f2566ed9
Mackay89 <manotomackay@gmail.com>
2024-02-27 14:41:43 -0800

    task

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -4,6 +4,6 @@ string = ARGV[0]
 matches = string.scan(/hb{2,5}n/)
 puts maches.join("\n")
 if ARGV.empty?
-  puts "Usage: #($/hb{2,5}n/) <string>"
+  puts "Usage: #{$/hb{2,5}n/} <string>"
   exit 1
 end 
