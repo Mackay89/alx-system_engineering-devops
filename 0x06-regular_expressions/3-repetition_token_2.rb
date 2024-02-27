9e34753ee0c6dfd47dce0afef763d71a046a8e85
Mackay89 <manotomackay@gmail.com>
2024-02-27 15:55:58 -0800

    task

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
-# This script accept one argument and passe it to a regular expression matching method
+# This script accept one argument and passes it to a regular expression matching method
 
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit 1
+end
 
 puts ARGV[0].scan(/hbt{n,}/).join
