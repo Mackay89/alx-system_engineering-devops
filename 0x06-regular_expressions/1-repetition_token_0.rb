84bd86325fa69ac269dacb41276592c2bad3107f
Mackay89 <manotomackay@gmail.com>
2024-02-27 14:36:20 -0800

    task

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,2 +1,9 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hb{2,5}n/).join
+
+string = ARGV[0]
+matches = string.scan(/hb{2,5}n/)
+puts maches.join("\n")
+if ARGV.empty?
+  puts "Usage: #($/hb{2,5}n/) <string>
+  exit 1
+end 
