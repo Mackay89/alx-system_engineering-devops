b7d535fc1bbc093304d9320f3f0e5b9c2acdcf53
Mackay89 <manotomackay@gmail.com>
2024-04-19 17:49:31 -0700

    task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 #install puppet-lint version 2.5.0
 
 exec { 'puppet-lint':
-	command => '/usr/bin/apt-get -y install puppet-lint -v2.5.0',
+	command => '/usr/bin/apt-get -y install puppet-lint',
+	unless  => '/usr/bin/dpkg -l | grep puppet-lint | grep -q "2.5.0"',
 }
 
