bd4567ab29f786d85bbe8436e504e7a570a9f410
Mackay89 <manotomackay@gmail.com>
2024-04-20 08:36:31 -0700

    task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,8 @@
-#Puppet manifest to install Flask from pip3
+# Puppet manifest to install Flask from pip3
+#
+# This manifest ensures that Flask version 2.0.1 is installed using pip3.
+
+# Define the package resource to install Flask
 package { 'flask':
   ensure   => '2.0.1',
   provider => 'pip3'
