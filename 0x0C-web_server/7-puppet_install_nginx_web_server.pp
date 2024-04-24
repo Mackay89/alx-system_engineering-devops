fe39ec9416fa25ed4b38e4eec75687887a3b5e48
Mackay89 <manotomackay@gmail.com>
2024-04-24 11:33:50 -0700

    task

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,39 @@
+#Script that defines a class for Nginx installation and configuration
+
+
+exec { "update system":
+	command => "/user/bin/apt-grt update",
+}
+
+
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system'],
+}
+
+
+file { '/var/www/html/index.html':
+	ensure  => file,
+	content => 'Hello World!',
+}
+
+
+file { '/etc/nginx/sites-available/default':
+	ensure => present,
+	content => template('nginx_redirect/nginx.conf.erb'),
+	notify => Service['nginx'],
+}
+
+
+file { '/etc/nginx/sites-enabled/default':
+	ensure => link,
+	target => '/etc/nginx/sites-available/default',
+	require => File['/etc/nginx/sites-available/default'],
+}
+
+
+service { 'nginx':
+	ensure => running,
+	enable => true,
+	require => Package['nginx']
+}
