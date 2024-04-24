fa85036873fb15c0d3768391662966f6de65ca3c
Mackay89 <manotomackay@gmail.com>
2024-04-24 14:50:54 -0700

    task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -23,18 +23,18 @@ File['/etc/nginx/sites-available/default'] ~> Service['nginx']
 
 #Check if Nginx root page returns "Hello World!"
 exec { 'check_nginx_root':
-  command	=> 'curl -s http://localhost/ | grep "Hello World!"',
-  unless	=> 'curl -s http://localhost/ | grep -q "Hello World"',
-  subscribe	=> File['/etc/nginx/sites-available/default'],
+  command   => 'curl -s http://localhost/ | grep "Hello World!"',
+  unless    => 'curl -s http://localhost/ | grep -q "Hello World"',
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
 
 
 #Add 301 redirection rule for /redirect_me
 file_line { 'add_301_redirection':
-  path	 => '/etc/nginx/sites-available/default',
-  line	 => '	rewrite ^/redirect_me https://github.com/luischaparroc permanent;',
-  match  => '^.*rewrite ^/redirect_me.*$',
-  notify => Service['nginx'],
+  path	  => '/etc/nginx/sites-available/default',
+  line	  => '	rewrite ^/redirect_me https://github.com/luischaparroc permanent;',
+  match   => '^.*rewrite ^/redirect_me.*$',
+  notify  => Service['nginx'],
   require => Package['nginx'],
 }
 
