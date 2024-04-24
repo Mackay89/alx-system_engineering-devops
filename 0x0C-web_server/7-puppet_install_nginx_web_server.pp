ce5f680a0c568c3f365c03d4b014270965eaf7d1
Mackay89 <manotomackay@gmail.com>
2024-04-24 14:01:57 -0700

    task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,39 +1,37 @@
-#Script that defines a class for Nginx installation and configuration
-
-
-exec { "update system":
-	command => "/user/bin/apt-get update",
-}
-
-
+#Install Nginx package
 package { 'nginx':
-	ensure => 'installed',
-	require => Exec['update system'],
-}
-
-
-file { '/var/www/html/index.html':
-	ensure  => file,
-	content => 'Hello World!',
+  ensure => installed,
 }
 
 
 file { '/etc/nginx/sites-available/default':
-	ensure => present,
-	content => template('nginx_redirect/nginx.conf.erb'),
-	notify => Service['nginx'],
-}
-
-
-file { '/etc/nginx/sites-enabled/default':
-	ensure => link,
-	target => '/etc/nginx/sites-available/default',
-	require => File['/etc/nginx/sites-available/default'],
+  ensure  => present,
+  content => template('nginx/default.erb'),
+  notify  => Service['nginx'],
 }
 
 
 service { 'nginx':
-	ensure => running,
-	enable => true,
-	require => Package['nginx']
+  ensure => running,
+  enable => true,
 }
+
+File['/etc/nginx/sites-available/default'] ~> Service['nginx']
+
+
+exec { 'check_nginx_root':
+  command	=> 'curl -s http://localhost/ | grep "Hello World!"',
+  unless	=> 'curl -s http://localhost/ | grep -q "Hello World"',
+  subscribe	=> File['/etc/nginx/sites-available/default'],
+}
+
+file_line { 'add_301_redirection':
+  path	 => '/etc/nginx/sites-available/default',
+  line	 => '	rewrite ^/redirect_me https://github.com/luischaparroc permanent;',
+  match  => '^.*rewrite ^/redirect_me.*$',
+  notify => Service['nginx'],
+  require => Package['nginx'],
+}
+
+
+File['/etc/nginx/sites-available/default'] ~> Service['nginx']
