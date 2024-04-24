11580c32aa6aeb798cd20288b93c666b971702f0
Mackay89 <manotomackay@gmail.com>
2024-04-24 14:20:46 -0700

    task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,7 +3,7 @@ package { 'nginx':
   ensure => installed,
 }
 
-
+#Confgure Nginx to listen on port 80
 file { '/etc/nginx/sites-available/default':
   ensure  => present,
   content => template('nginx/default.erb'),
@@ -11,20 +11,25 @@ file { '/etc/nginx/sites-available/default':
 }
 
 
+#Ensure Nginx service is running
 service { 'nginx':
   ensure => running,
   enable => true,
 }
 
+
+#Ensure Nginx is restarted when  configuration changes
 File['/etc/nginx/sites-available/default'] ~> Service['nginx']
 
-
+#Check if Nginx root page returns "Hello World!"
 exec { 'check_nginx_root':
   command	=> 'curl -s http://localhost/ | grep "Hello World!"',
   unless	=> 'curl -s http://localhost/ | grep -q "Hello World"',
   subscribe	=> File['/etc/nginx/sites-available/default'],
 }
 
+
+#Add 301 redirection rule for /redirect_me
 file_line { 'add_301_redirection':
   path	 => '/etc/nginx/sites-available/default',
   line	 => '	rewrite ^/redirect_me https://github.com/luischaparroc permanent;',
@@ -33,5 +38,5 @@ file_line { 'add_301_redirection':
   require => Package['nginx'],
 }
 
-
+#Ensure Nginx is restarted when 301 redirection rule changes
 File['/etc/nginx/sites-available/default'] ~> Service['nginx']
