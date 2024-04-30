443befdb174aa2b2a75a22ef1e98d44202ce84e3
Mackay89 <manotomackay@gmail.com>
2024-04-29 17:23:54 -0700

    task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,29 +1,30 @@
 #Update package respositories
 exec {'update':
-	command => '/usr/bin/apt-get update',
+  command => '/usr/bin/apt-get update',
 }
 
 
 #Install Nginx package
 package {'nginx':
-	ensure => 'present',
+  ensure => 'present',
 }
 
 Facter.add('nginx_hostname') do
-	setcode do 
-		Facter::Core::Execution.execute('hostname')
-	end
+  setcode do 
+    Facter::Core::Execution.execute('hostname')
+  end
 end
 
 
 file_line { 'http_header':
-	path  => '/etc/nginx/nginx.conf',
-	line  => " add_header X-Served-By \"${facts['nginx_hostname']}\";",
-	match => 'http {',
+  path   => '/etc/nginx/nginx.conf',
+  line   => " add_header X-Served-By \"${facts['nginx_hostname']}\";",
+  match  => 'http {',
+  notify => Exec['nginx_restart'],
 }
+
 
 # Restart Nginx service
 exec {'nginx_restart':
-	command => '/usr/sbin/service nginx restart',
-	require => File_line['http_header']
+  command => '/usr/sbin/service nginx restart',
 }
