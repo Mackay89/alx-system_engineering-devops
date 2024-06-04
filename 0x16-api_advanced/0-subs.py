a37df5d9ce993220bf7c6e9e189417ac7f3d3258
Mackay89 <manotomackay@gmail.com>
2024-06-04 12:58:27 -0700

    task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,7 @@ def number_of_subscribers(subreddit):
     Queries the Reddit API and returns the number of subscribers for a given subreddit.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'User_agent 0.1'}
+    headers = {'User-Agent': 'my-app/0.1'}
     try:
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
