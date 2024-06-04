60bc262e02101bb2e161f4be0c863282d88df336
Mackay89 <manotomackay@gmail.com>
2024-06-04 12:28:32 -0700

    task

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This script contains a function to query the Reddit API and return 
+the number of subscribers for a given subreddit.
+"""
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'User_agent 0.1'}
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data.get['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Usage: ./0-subs.py <subreddit>")
+    else:
+        subreddit = sys.argv[1]
+        print(number_of_subscribers(subreddit))
