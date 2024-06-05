40ab824e8e2ecfee2328be4097593f173b003b4d
Mackay89 <manotomackay@gmail.com>
2024-06-05 02:51:49 -0700

    task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -47,5 +47,9 @@ def top_ten(subreddit):
             print(item.get('data', {}).get('title', "None"))
 
 
-    except Exception as e:
+    except Exception:
         print("None")
+
+
+if __name__ == "__main__":
+    topten('python')
