6a4b2fc1a17a30ff4dd1ce73ee767e3b7fd7278e
Mackay89 <manotomackay@gmail.com>
2024-06-05 05:01:46 -0700

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -4,4 +4,4 @@
 exec { 'fix_typo':
   path	   => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   command  =>  "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
-  privider => 'shell'}
+  provider => 'shell'}
