7ab02fad5d6c1c3c49dc2a875a01811f22f0aa2c
Mackay89 <manotomackay@gmail.com>
2024-06-09 11:20:12 -0700

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
-#Using strace and then using Puppet to automate fix for 500 error on Apache2 
+# This Puppet manifest fixes atypo in wp-settings.php to resolve a 500 error on Apache2
+
 exec { 'fix_typo':
-  path	   => ['/usr/bin', '/bin', '/bin', '/usr/bin'],
+  path     => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
   command  =>  "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
   provider => 'shell',
 }
