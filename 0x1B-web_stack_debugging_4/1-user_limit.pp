3ce4c468106d8a1d8a6216e77982f318746376e5
Mackay89 <manotomackay@gmail.com>
2024-06-13 01:41:58 -0700

    task

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,6 @@
-exec { 'change-os-configuration-for-holberton-user':
-  command => '/bin/echo -e "holberton soft nofile 4096\nholberton hard nofile 8192" >> /etc/security/limits.conf && /bin/echo "session required pam_limits.so" >> /etc/pam.d/common-session-noninteractive',
-  unless +> 'grep -q "holberton" /etc/security/limits.conf',
+# Login with the holberton user and open a file without any error message
+file { 'loginFile':
+  ensure  => present,
+  path    => '/etc/security/limits.conf',
+  content => '# File erased'
 }
